090c21773e2547a9d7613cb1981e8786ea559457
CaxaROCK <yakubueric@gmail.com>
2023-12-06 13:51:15 +0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
